60cb8070816e4b7691e71cdfc6e7db4d3aef3335
Vasu Grover <notcoderguy@gmail.com>
2024-10-01 16:52:23 +0530

    update: updated styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3ebff8',
+      }
+    },
   },
   variants: {
     extend: {},
@@ -17,7 +21,22 @@ module.exports = {
   ],
   // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "primary": "#3ebff8",
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          "primary": "#3ebff8",
+          "base-100": "#000000",
+          "base-200": "#3C3C3C",
+          "base-300": "#4C4C4C",
+          "base-content": "#FFFFFF",
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
